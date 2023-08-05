58d1bb23efc036ea9f3d26c57b51a8f17e276e0e
euhurias <fernandohurias@ufpi.edu.br>
2023-08-05 17:19:09 -0300

    Arualização

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+//
+// Configure the HTTP request pipeline....
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
